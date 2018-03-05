c7181e059b0715196b6a93df2287e99a58c6cb47
RandyField <287572291@qq.com>
2018-03-05 09:01:26 +0800

    all

--- BLL/DapperDataBaseConfig.cs
+++ BLL/DapperDataBaseConfig.cs
@@ -10,7 +10,7 @@ namespace BLL
 {
     public class DapperDataBaseConfig
     {
-        private static string DefaultMySqlConnectionString=@"Server=localhost;Database=School;user=root;Password=randy1992;pooling=true;CharSet=utf8;port=3306;sslmode=none";
+        private static string DefaultMySqlConnectionString=@"Server=localhost;Database=School;user=root;Password=a287572291;pooling=true;CharSet=utf8;port=3306;sslmode=none";
 
 
         //数据库配置
